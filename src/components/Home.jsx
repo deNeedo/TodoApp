ceeed683befba82bc33e5653315a0f8a9d627b66
deNeedo <jkudrys@protonmail.com>
2024-06-09 15:27:14 +0200

    Implemented login and registration process. No frontend, only pure functionality.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
-import {collection, query, onSnapshot, doc, updateDoc, deleteDoc, QuerySnapshot} from 'firebase/firestore'
+import { useNavigate } from 'react-router-dom'
+import { collection, query, onSnapshot, doc, updateDoc, deleteDoc, QuerySnapshot } from 'firebase/firestore'
 import Title from './Title.jsx'
 import Todo from './Todo.jsx'
 import AddTodo from './AddTodo.jsx'
-import {db} from '../firebase.js'
+import { db } from '../firebase.js'
 import './style/Home.css'
 
 
 export default function Home() {
     const [todos, setTodos] = React.useState([])
+    const navigate = useNavigate()
+    const loginRedirect = () => {navigate('/login')}
+    const registerRedirect = () => {navigate('/register')}
 
     React.useEffect(() => {
-        const q = query(collection(db, "todos"))
+        const q = query(collection(db, "todos")) // if you want to see users instead of todos change here to "users"
         const unsub = onSnapshot(q, (QuerySnapshot) => {
             let todosArray = []
             QuerySnapshot.forEach((doc) => {
@@ -29,7 +33,7 @@ export default function Home() {
         await updateDoc(doc(db, "todos", todo.id), {completed: !todo.completed})
     }
     const handleDelete = async (id) => {
-        await deleteDoc(doc(db, "todos", id))
+        await deleteDoc(doc(db, "todos", id)) // if you want to delete users instead of todos change here to "users"
     }
 
 
@@ -41,6 +45,8 @@ export default function Home() {
             <div>
                 <AddTodo/>
             </div>
+            <div> <button onClick={registerRedirect}> REGISTER REDIRECT </button> </div>
+            <div> <button onClick={loginRedirect}> LOGIN REDIRECT </button> </div>
             <div>
                 {todos.map((todo) => (
                     <Todo key={todo.id} todo={todo} handleEdit={handleEdit} toggleComplete={toggleComplete} handleDelete={handleDelete}/>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,15 +1,43 @@
 import React from 'react'
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import { loginValidator } from '../utils/loginValidator'
 import './style/Login.css'
 
 export default function Login() {
+    const [username, setUsername] = React.useState("")
+    const [password, setPassword] = React.useState("")
     const navigate = useNavigate()
     const registerRedirect = () => {navigate('/register')}
+    const homeRedirect = () => {navigate('/home')}
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        if (username !== "" & password !== "") {
+            let response
+            let promise = loginValidator(username, password)
+            await promise
+                .then((value) => {response = value})
+                .catch(() => {console.log('Unexpected error occured!')})
+            if (response == 0) {
+                console.log('Successfully logged in!')
+                homeRedirect()
+            } else if (response == 1) {
+                console.log('Wrong credentials!')
+            }
+            setUsername(""); setPassword("")
+        }
+        else {
+            // handle locking button if not all fields contain data
+        }
+    }
     return (
         <div>
-            <div id="login_test">
-                <button onClick={registerRedirect}> REGISTER REDIRECT </button>
-            </div>
+            <form onSubmit={handleSubmit}>
+                <div> <input type='test' placeholder='Enter username...' value={username} onChange={(e) => setUsername(e.target.value)}/> </div>
+                <div> <input type='test' placeholder='Enter password...' value={password} onChange={(e) => setPassword(e.target.value)}/> </div>
+                <div> <button> Login </button> </div>
+            </form>
+            <div> <button onClick={registerRedirect}> REGISTER REDIRECT </button> </div>
+            <div> <button onClick={homeRedirect}> HOME REDIRECT </button> </div>
         </div>
     )
 }
